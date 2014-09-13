0c4c79fdb3f00f6bf515121cc7e65bb979dbd1e4
stanleyn <stanleyhlng@yahoo.com.hk>
2014-09-13 01:38:49 -0700

    implement scrollview

--- ios8_rotten_tomatoes/MovieViewController.swift
+++ ios8_rotten_tomatoes/MovieViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class MovieViewController: UIViewController {
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
     var movie: Movie!
     
     override func viewDidLoad() {
@@ -19,12 +23,39 @@ class MovieViewController: UIViewController {
         println("MovieViewController - viewDidLoad")
         dump(movie);
         
-        // nav title
         if movie != nil {
-            self.navigationItem.title = movie.title
-        }
-    }
 
+            // nav.title
+            //navigationItem.title = movie.title
+            
+            // content.title
+            titleLabel.text = movie.title
+            titleLabel.sizeToFit()
+            println("titleLabel.frame.size = \(titleLabel.frame.size)")
+            
+            // content.synopsis
+            synopsisLabel.text = movie.synopsis
+            synopsisLabel.sizeToFit()
+            synopsisLabel.frame.origin.y = titleLabel.frame.origin.y
+                + titleLabel.frame.size.height
+                + 50
+            println("synopsisLabel.frame.size = \(synopsisLabel.frame.size)")
+            
+            // content
+            contentView.sizeToFit()
+            println("contentView.frame.size = \(contentView.frame.size)")
+        }
+
+        // current screen size
+        var bounds = UIScreen.mainScreen().bounds
+        
+        // configure scroll view
+        var insetTop = bounds.height - titleLabel.frame.height - 80
+        scrollView.contentInset = UIEdgeInsetsMake(insetTop, 0, 0, 0)
+        scrollView.contentSize = contentView.frame.size
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
